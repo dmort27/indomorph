488778a303829a811ea89bf2e9fda1317c049ea7
David R. Mortensen <davidmortensen@gmail.com>
2018-07-18 18:27:57 -0400

    Added preliminary interface

---
+++ indomorph/for_embedding.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import mmap
+import fstinter
+import glob
+import os.path
+import sys
+import tqdm
+
+def get_num_lines(file_path):
+    fp = open(file_path, "r+")
+    buf = mmap.mmap(fp.fileno(), 0)
+    lines = 0
+    while buf.readline():
+        lines += 1
+    return lines
+
+def get_cost(aml):
+    analysis, morphs, lemma = aml
+    c = 0
+    if 'Nom' in morphs:
+        c += 1
+    if len(lemma) < 2:
+        c += 4
+    return c
+
+def main(fstpath, path_in, path_out):
+    fst = fstinter.FST(fstpath, get_cost)
+    for fnin in glob.glob(os.path.join(path_in, '*')):
+        fnout = os.path.join(path_out, os.path.basename(fnin))
+        with open(fnin, encoding='utf-8') as fin, open(fnout, 'w', encoding='utf-8') as fout:
+            for line in tqdm.tqdm(fin, total=get_num_lines(fnin)):
+                output = []
+                tokens = line.strip().split()
+                analyses = fst.analyses(tokens)
+                for token, (morphs, lemma) in zip(tokens, analyses):
+                    props = [] if len(morphs) < 2 else morphs[1:]
+                    output.append('w:{}~l:{}~m:{}'.format(token,
+                                                          lemma,
+                                                          '+'.join(props)))
+                print(' '.join(output), file=fout)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
---
+++ indomorph/fstinter.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+from subprocess import Popen, PIPE
+import regex as re
+
+class FST(object):
+    def __init__(self, fst=None, get_cost=None):
+        self.fst = fst
+        self._get_cost = get_cost
+
+    def _to_morphs(self, analysis):
+        try:
+            morphs = analysis.split('+')
+            if morphs[0] == '':
+                return morphs[1:]
+            else:
+                return morphs
+        except IndexError:
+            return []
+
+    def _get_lemma(self, morphs):
+        return morphs[0]
+
+    def _get_best_analysis(self, analyses):
+        morph_list = [self._to_morphs(x) for x in analyses]
+        lemma_list = [self._get_lemma(x) for x in morph_list]
+        costs = [self._get_cost(x)
+                 for x in zip(analyses, morph_list, lemma_list)]
+        try:
+            sorted_list = sorted(zip(list(costs), morph_list, lemma_list))
+            _, morphs, lemma = sorted_list[0]
+            return (morphs, lemma)
+        except IndexError:
+            return (None, None)
+
+    def analyses(self, tokens):
+        token_list = '\n'.join(tokens)
+        pipe = Popen(['flookup', '-a', '-x', self.fst],
+                     stdout=PIPE, stdin=PIPE)
+        analyses, resp = pipe.communicate(input=token_list.encode('utf-8'))
+        analyses = analyses.decode('utf-8').strip()
+        analysis_list = []
+        for token, token_analyses in zip(tokens, analyses.split('\n\n')):
+            morphs, lemma = self._get_best_analysis(token_analyses.split('\n'))
+            if lemma:
+                analysis_list.append((morphs, lemma))
+            else:
+                analysis_list.append(([token], token))
+        return zip(tokens, analysis_list)
