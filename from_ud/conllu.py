9e9d6b41ae76cdc3204492f0d5807b06ec0f14c1
David R. Mortensen <dmortens@cs.cmu.edu>
2019-04-23 15:57:01 -0400

    Added transform to conllu parser

--- from_ud/conllu.py
+++ from_ud/conllu.py
@@ -2,6 +2,16 @@
 
 import re
 
+
+def morph_to_dict(props):
+    return {k: v for (k,v)
+            in [x.split('=') for x in props.split('|')]}
+
+
+def lemma_morph_tr(token):
+    return (token[3], morph_to_dict(token[5]))
+
+
 class ConllU:
     def __init__(self, fn, transform):
         self.fn = fn
@@ -12,11 +22,12 @@ class ConllU:
         with open(self.fn, encoding='utf-8') as f:
             for word in f:
                 word = word[:-1]
-                word = re.sub('#.*', '', word)
-                if re.match('^\s*$', word):
+                if re.match('#.*', word):
+                    continue
+                elif re.match('^\s*$', word):
                     if sentence:
-                        yield list(map(self.transform, sentence))
+                        yield sentence
                         sentence = []
                 else:
-                    sentence.append(word.split('\t'))
+                    sentence.append(self.transform(word.split('\t')))
             yield sentence
